b78eb0e55c4ff8a07619aed18fb7d736d7268ccc
Emmanuel Livingstone <livingstone.s.e@gmail.com>
2014-07-08 06:45:03 +0530

    Fixed srm625 div2 level3 problem.

--- src/main/java/srm625/division2/ConundrumReloaded.java
+++ src/main/java/srm625/division2/ConundrumReloaded.java
@@ -1,49 +1,73 @@
 package srm625.division2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Level 3 problem.
+ * Level 3 Problem
  * 
  * @author emmanuel
  *
  */
 public class ConundrumReloaded {
-  
-  public int minimumLiars(String answers)
-  {
-      int minLiars = Integer.MAX_VALUE;
-      boolean feasible, jLiar, jp1Liar ;
-      for (long i=0;i<1<<answers.length();i++) {
-        int liarCount = 0;
-        for (int j=0;j<answers.length();j++)
-          if ((i & (1<<j)) == 0)
-            liarCount++;
-        feasible = true;
-        for (int j=0;j<answers.length();j++) {
-          jLiar = ((i & (1<<j)) == 0);
-          jp1Liar = ((i & (1<<((j+1) % answers.length()))) == 0);
-          if (jLiar) {
-            if ((answers.charAt(j) == 'H' && jp1Liar) || (answers.charAt(j) == 'L' && !jp1Liar) || answers.charAt(j) == '?')
-              continue;
-            else {
-              feasible = false;
-              break;
-            }
-          }
-          else {
-            if ((answers.charAt(j) == 'L' && jp1Liar) || (answers.charAt(j) == 'H' && !jp1Liar) || answers.charAt(j) == '?')
-              continue;
-            else {
-              feasible = false;
-              break;
-            }
-          }
+  private int minimumLiarsLine(List<Character> arr) {
+    boolean isHonest;
+    int minLiars = Integer.MAX_VALUE, liarCount;
+    for (int i=0;i<2;i++) {
+      isHonest = i == 0;
+      liarCount = 0;
+      for (Character c : arr)
+        if (isHonest)
+          isHonest = c == 'H';
+        else {
+          isHonest = c == 'L';
+          liarCount++;
         }
-        if (feasible && liarCount < minLiars)
-          minLiars = liarCount;
+      if (!isHonest)
+        liarCount++;
+      if (liarCount < minLiars)
+        minLiars = liarCount;
+    }
+    return minLiars;
+  }
+
+  private int minimumLiarsCyclic(boolean isFirstPersonHonest, String answers) {
+    boolean isHonest = isFirstPersonHonest;
+    int liarCount = 0;
+    for (int i = 0; i < answers.length(); i++) {
+      if (isHonest)
+        isHonest = answers.charAt(i) == 'H';
+      else {
+        isHonest = answers.charAt(i) == 'L';
+        liarCount++;
       }
-      if (minLiars == Integer.MAX_VALUE)
-        return -1;
-      return minLiars;
+    }
+    if (isFirstPersonHonest == isHonest)
+      return liarCount;
+    return Integer.MAX_VALUE;
+  }
+
+  public int minimumLiars(String answers) {
+    int minLiars = Integer.MAX_VALUE, liarCount;
+    if (answers.contains("?")) {
+      liarCount = 0;
+      int firstUnknownIndex = answers.indexOf('?', 0), i = (firstUnknownIndex + 1) % answers.length();
+      while (i != firstUnknownIndex) {
+        while (answers.charAt(i) == '?' && i != firstUnknownIndex)
+          i = (i + 1) % answers.length();
+        List<Character> arr = new ArrayList<>();
+        while (answers.charAt(i) != '?') {
+          arr.add(answers.charAt(i));
+          i = (i + 1) % answers.length();
+        }
+        liarCount += minimumLiarsLine(arr);
+      }
+      minLiars = liarCount;
+    } else {
+      minLiars = Math.min(minLiars, minimumLiarsCyclic(true, answers));
+      minLiars = Math.min(minLiars, minimumLiarsCyclic(false, answers));
+    }
+    return minLiars == Integer.MAX_VALUE ? -1 : minLiars;
   }
 
   public static void main(String[] args) {
@@ -53,6 +77,6 @@ public class ConundrumReloaded {
     System.out.println(obj.minimumLiars("LHLH?"));
     System.out.println(obj.minimumLiars("??LLLLLL??"));
     System.out.println(obj.minimumLiars("LLL"));
+    System.out.println(obj.minimumLiars("?L?L?L?L?L"));
   }
-
 }
