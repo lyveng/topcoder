ac4b62b86e63d2e659ec716c5d1ee76640b962d3
Livingstone S E <livingstone.s.e@gmail.com>
2017-01-08 12:36:22 +0530

    Graph Helper classes

---
+++ helpers/src/DirectedEdge.java
@@ -0,0 +1,26 @@
+public class DirectedEdge {
+    private final int v, w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+}
